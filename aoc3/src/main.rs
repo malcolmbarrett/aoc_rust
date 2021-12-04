aac955f85aabf50fd913972d47235f29fc22babc
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-12-03 18:43:10 -0800

    add part 2

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -2,19 +2,58 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() {
-    let data: Vec<f64> = read_input()
+    let mut input = read_input();
+    let data: Vec<f64> = transpose(input.clone())
         .into_iter()
         .map(|col|  col.iter().sum::<i32>() as f64 / col.len() as f64)
         .collect();
 
-    let gamma: Vec<i32> = data.iter().map(|x| if x > &0.50 { 1 } else { 0 }).collect();
+    let gamma: Vec<i32> = data.iter().map(|x| if x >= &0.50 { 1 } else { 0 }).collect();
     let epsilon: Vec<i32> = gamma.iter().map(|x| if x == &1 { 0 } else { 1 }).collect();
 
+    let oxygen = filter_codes(&input, "gamma");
+    let co2_scrubber = filter_codes(&input, "epsilon");
+
     println!("Part 1: {}", solve(gamma, epsilon));
+    println!("Part 2: {}", solve(oxygen, co2_scrubber));
+
 }
 
-fn solve(gamma: Vec<i32>, epsilon: Vec<i32>) -> isize {
-    binary_to_decimal(&vec_to_single(gamma)) * binary_to_decimal(&vec_to_single(epsilon))
+fn filter_codes(codes: &Vec<Vec<i32>>, category: &str) -> Vec<i32> {
+    let mut codes = codes.clone();
+
+
+    for i in 0..12 {
+        let data: Vec<f64> = transpose(codes.clone())
+            .into_iter()
+            .map(|col|  col.iter().sum::<i32>() as f64 / col.len() as f64)
+            .collect();
+
+        let gamma: Vec<i32> = data.iter().map(|x| if x >= &0.50 { 1 } else { 0 }).collect();
+        let epsilon: Vec<i32> = gamma.iter().map(|x| if x == &1 { 0 } else { 1 }).collect();
+
+        let counts = match category {
+            "gamma" => gamma,
+            "epsilon" => epsilon,
+            _ => panic!("Wrong category")
+        };
+
+        codes = codes
+            .iter()
+            .filter(|x| x[i] == counts[i])
+            .cloned()
+            .collect();
+
+        if codes.len() == 1 {
+            break;
+        }
+    }
+
+    codes[0].clone()
+}
+
+fn solve(x: Vec<i32>, y: Vec<i32>) -> isize {
+    binary_to_decimal(&vec_to_single(x)) * binary_to_decimal(&vec_to_single(y))
 }
 
 fn binary_to_decimal(binary: &str) -> isize {
@@ -45,7 +84,7 @@ fn read_input() -> Vec<Vec<i32>> {
         lines.push(vec);
     };
 
-    transpose(lines)
+    lines
 }
 
 // from https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
