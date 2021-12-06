ab8f84972450d2b43ffeb02fbeb4c68c20a22a1a
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-12-05 20:16:56 -0800

    a little refactoring

--- aoc5/src/main.rs
+++ aoc5/src/main.rs
@@ -1,12 +1,22 @@
-use std::fs;
 use std::collections::HashMap;
+use std::fs;
 
 type Coords = Vec<Vec<Vec<i32>>>;
 type Pair = Vec<Vec<i32>>;
 type Point = (i32, i32);
 
 fn main() {
-    let contents: Coords = fs::read_to_string("input.txt")
+    let contents = read_input();
+
+    let points_part_1 = extend_coords(&contents, false);
+    let points_part_2 = extend_coords(&contents, true);
+
+    println!("Part 1: {}", count_points(points_part_1));
+    println!("Part 2: {}", count_points(points_part_2));
+}
+
+fn read_input() -> Coords {
+    fs::read_to_string("input.txt")
         .unwrap()
         .lines()
         .map(|line| {
@@ -14,34 +24,25 @@ fn main() {
                 .map(|s| s.split(",").map(|s| s.parse::<i32>().unwrap()).collect())
                 .collect()
         })
-        .collect();
-
-    let points = extend_coords(&contents, false);
-    let points2 = extend_coords(&contents, true);
-
-    println!("Part 1: {}", count_points(points));
-    println!("Part 2: {}", count_points(points2));
+        .collect()
 }
 
 fn count_points(points: Vec<Point>) -> usize {
-    let mut value_counts : HashMap<Point, i32> = HashMap::new();
+    let mut value_counts: HashMap<Point, i32> = HashMap::new();
 
     for point in points.iter() {
         *value_counts.entry(*point).or_insert(0) += 1;
-    };
-    
-    value_counts
-        .into_iter()
-        .filter(|x| x.1 > 1)
-        .count()
+    }
+
+    value_counts.into_iter().filter(|x| x.1 > 1).count()
 }
 
 fn extend_coords(coords: &Coords, count_diagonals: bool) -> Vec<Point> {
     let mut expanded_coords = Vec::new();
     for pair in coords {
-        if pair[0][0] == pair[1][0] {
+        if is_vertical(&pair) {
             expanded_coords.append(&mut extend_vertical_coords(pair.to_vec()));
-        } else if pair[0][1] == pair[1][1] {
+        } else if is_horizontal(&pair) {
             expanded_coords.append(&mut extend_horizontal_coords(pair.to_vec()));
         } else if count_diagonals {
             expanded_coords.append(&mut extend_diagonal_coords(pair.to_vec()));
@@ -49,6 +50,14 @@ fn extend_coords(coords: &Coords, count_diagonals: bool) -> Vec<Point> {
     }
 
     expanded_coords
+}
+
+fn is_vertical(pair: &Pair) -> bool {
+    pair[0][0] == pair[1][0]
+}
+
+fn is_horizontal(pair: &Pair) -> bool {
+    pair[0][0] == pair[1][0]
 }
 
 fn extend_vertical_coords(pair: Pair) -> Vec<Point> {
@@ -81,7 +90,6 @@ fn extend_horizontal_coords(pair: Pair) -> Vec<Point> {
         }
     }
 
-
     points
 }
 
@@ -92,44 +100,23 @@ fn explode_points(pair: Pair) -> (i32, i32, i32, i32) {
 fn extend_diagonal_coords(pair: Pair) -> Vec<Point> {
     let (x_start, y_start, x_end, y_end) = explode_points(pair);
     let mut points: Vec<Point> = Vec::new();
+    let mut x: Vec<i32> = Vec::new();
+    let mut y: Vec<i32> = Vec::new();
 
-    // up and right
-    if x_start < x_end && y_start < y_end {
-        let x: Vec<i32> = (x_start..=x_end).collect();
-        let y: Vec<i32> = (y_start..=y_end).collect();
-
-        for i in 0..x.len() {
-            points.push((x[i], y[i]));
-        }
-
-    // down and right
-    } else if x_start < x_end && y_start > y_end {
-        let x: Vec<i32> = (x_start..=x_end).collect();
-        let y: Vec<i32> = (y_end..=y_start).rev().collect();
-
-        for i in 0..x.len() {
-            points.push((x[i], y[i]));
-        }
-
-    // down and left
-    } else if x_start > x_end && y_start > y_end {
-        let x: Vec<i32> = (x_end..=x_start).rev().collect();
-        let y: Vec<i32> = (y_end..=y_start).rev().collect();
-
-        for i in 0..x.len() {
-            points.push((x[i], y[i]));
-        }
-
-    // up and left
-    } else if x_start > x_end && y_start < y_end {
-        let x: Vec<i32> = (x_end..=x_start).rev().collect();
-        let y: Vec<i32> = (y_start..=y_end).collect();
-
-        for i in 0..x.len() {
-            points.push((x[i], y[i]));
-        }
+    if x_start < x_end {
+        x.append(&mut (x_start..=x_end).collect());
     } else {
-        panic!("direction not supported")
+        x.append(&mut (x_end..=x_start).rev().collect());
+    }
+
+    if y_start < y_end {
+        y.append(&mut (y_start..=y_end).collect());
+    } else {
+        y.append(&mut (y_end..=y_start).rev().collect());
+    }
+
+    for i in 0..x.len() {
+        points.push((x[i], y[i]));
     }
 
     points
@@ -138,22 +125,40 @@ fn extend_diagonal_coords(pair: Pair) -> Vec<Point> {
 #[test]
 fn test_extend_coords() {
     let vertical: Pair = vec![vec![1, 1], vec![1, 3]];
-    assert_eq!(extend_vertical_coords(vertical), vec![(1, 1), (1, 2), (1, 3)]);
+    assert_eq!(
+        extend_vertical_coords(vertical),
+        vec![(1, 1), (1, 2), (1, 3)]
+    );
 
     let vertical_rev: Pair = vec![vec![1, 3], vec![1, 1]];
-    assert_eq!(extend_vertical_coords(vertical_rev), vec![(1, 3), (1, 2), (1, 1)]);
+    assert_eq!(
+        extend_vertical_coords(vertical_rev),
+        vec![(1, 3), (1, 2), (1, 1)]
+    );
 
     let horizontal: Pair = vec![vec![7, 7], vec![9, 7]];
-    assert_eq!(extend_horizontal_coords(horizontal), vec![(7, 7), (8, 7), (9, 7)]);
+    assert_eq!(
+        extend_horizontal_coords(horizontal),
+        vec![(7, 7), (8, 7), (9, 7)]
+    );
 
     let horizontal_rev: Pair = vec![vec![9, 7], vec![7, 7]];
-    assert_eq!(extend_horizontal_coords(horizontal_rev), vec![(9, 7), (8, 7), (7, 7)]);
+    assert_eq!(
+        extend_horizontal_coords(horizontal_rev),
+        vec![(9, 7), (8, 7), (7, 7)]
+    );
 
     let diagonal_up_right: Pair = vec![vec![1, 1], vec![3, 3]];
-    assert_eq!(extend_diagonal_coords(diagonal_up_right), vec![(1, 1), (2, 2), (3, 3)]);
-    
+    assert_eq!(
+        extend_diagonal_coords(diagonal_up_right),
+        vec![(1, 1), (2, 2), (3, 3)]
+    );
+
     let diagonal_up_left: Pair = vec![vec![9, 7], vec![7, 9]];
-    assert_eq!(extend_diagonal_coords(diagonal_up_left), vec![(9, 7), (8, 8), (7, 9)]);
+    assert_eq!(
+        extend_diagonal_coords(diagonal_up_left),
+        vec![(9, 7), (8, 8), (7, 9)]
+    );
 }
 
 #[test]
